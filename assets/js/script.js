cd3ea1dce0fe67cb2532ee3bebcf5103d98d7710
Fernando Donoso <fidonoso@gmail.com>
2022-01-20 22:31:03 -0300

    Desafio GitHub Final v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,70 +20,76 @@ const getUser = async(userName)=>{
 var btnEnviar=document.querySelector('button')
 //operacion
 btnEnviar.addEventListener('click',(e)=>{
-    // alert("hola")
     e.preventDefault();
     const resultados= document.getElementById('resultados')
+    resultados.innerHTML=''
     const usernom=document.getElementById('nombre').value
     const Npag=parseInt(document.getElementById('pagina').value)
     const Nrepo=parseInt(document.getElementById('repoPagina').value)
     if(usernom =='' || Npag=='' || Nrepo==''){
         return alert("Complete todos los campos")
     }
-    // alert("hola")
-   
+
     Promise.all([getUser(usernom),getRepo(usernom, Npag, Nrepo)]).then(res=>{
-        // var datosUsuario=res[0]
-        var datosRepos=res[1].map(p=>p.name)
-        // console.log(datosUsuario)
-        console.log(datosRepos)
+        var datosRepos=res[1].map(p=> [p.name, p.html_url] )
+        // console.log(datosRepos)
         var avatarURL = res[0].avatar_url
-        // var nombreUsuario = res[0].name
-        // var loginUsuario = res[0].login
-        // var numeroRepos = res[0].public_repos
-        // var ubicacion = res[0].location
-        // var typeuser = res[0].type
-        //  console.log(avatarURL, nombreUsuario, loginUsuario, numeroRepos, ubicacion, typeuser)
-         var div= document.createElement('div')
-         div.className = "row"
-         resultados.appendChild(div)
-         var divI= document.createElement('div')
-         divI.className = "col-12 col-sm-6 ms-0"
-         divI.id='datosUsuario'
-         div.appendChild(divI)
-         var h2 =document.createElement('h3')
-         h2.innerHTML="Datos de Usuario"
-         divI.appendChild(h2)
+        var div= document.createElement('div')
+        div.className = "row"
+        resultados.appendChild(div)
+        var divI= document.createElement('div')
+        divI.className = "col-12 col-sm-6 ms-0"
+        divI.id='datosUsuario'
+        div.appendChild(divI)
+        var h3 =document.createElement('h3')
+        h3.innerHTML="Datos de Usuario"
+        divI.appendChild(h3)
 
-         var divD= document.createElement('div')
-         divD.className = "col-12 col-sm-6 me-0"
-         divD.id='datosRepos'
-         div.appendChild(divD)
-         var h2 =document.createElement('h3')
-         h2.innerHTML="Nombre de Repositorios"
-         divD.appendChild(h2)
+        var divD= document.createElement('div')
+        divD.className = "col-12 col-sm-6 "
+        divD.id='datosRepos'
+        div.appendChild(divD)
+        var h3 =document.createElement('h3')
+        h3.className="text-right"
+        h3.innerHTML="Nombre de Repositorios"
+        divD.appendChild(h3)
 
-         var im= document.createElement('img')
-         im.setAttribute('src', avatarURL)
-         im.style.width="200px"
-         divI.appendChild(im)
+        var im= document.createElement('img')
+        im.setAttribute('src', avatarURL)
+        im.style.width="150px"
+        divI.appendChild(im)
         
-         var datosUsers=[`Nombre de usuario: ${res[0].name}`, `Nombre de login: ${res[0].login}`, `Cantidad de Repositorios: ${res[0].public_repos}`, `Localidad: ${res[0].location}`, `Tipo de Usuario: ${res[0].type}`]
+        var datosUsers=[`Nombre de usuario: ${res[0].name}`, `Nombre de login: ${res[0].login}`, `Cantidad de Repositorios: ${res[0].public_repos}`, `Localidad: ${res[0].location}`, `Tipo de Usuario: ${res[0].type}`]
         datosUsers.forEach(el=>{
             var p =document.createElement('p')
             p.innerHTML=el
             divI.appendChild(p)
         })    
-         datosRepos.forEach(el => {
-             var p =document.createElement('p')
-             p.innerHTML=el
-             divD.appendChild(p)
-         });
-
+        datosRepos.forEach(el => {
+            var p =document.createElement('p')
+            p.className="text-right"
+            var a= document.createElement('a')
+            a.innerHTML=el[0]
+            a.setAttribute('href', el[1])
+            a.setAttribute('target','_blank')
+            p.appendChild(a)
+            divD.appendChild(p)
+        });
+        var inputs= document.querySelectorAll('input')
+        inputs.forEach(el=>{
+            el.value=''
+        })
         
-    }).catch(err => console.log('err', err))
-
-
-
+    }).catch(err => {
+        console.log(err.message)
+        console.log(err.name)
+        console.log(err.number)
+        alert("Algo salió mal. Verifique el nombre de usuario de GitHub")
+        var inputs= document.querySelectorAll('input')
+        inputs.forEach(el=>{
+            el.value=''
+        })
+    })
 
 })
 
